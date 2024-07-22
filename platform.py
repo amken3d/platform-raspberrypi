8eed4a65ad911678a925fa8d0b03f7da9412ab6e
Amken USA <166057890+amken3d@users.noreply.github.com>
2024-07-21 22:47:23 -0400

    Update platform.py

--- platform.py
+++ platform.py
@@ -83,6 +83,13 @@ class RaspberrypiPlatform(PlatformBase):
                 self.packages["toolchain-rp2040-earlephilhower"]["optional"] = False
                 # Configure toolchain download link dynamically
                 self.packages["toolchain-rp2040-earlephilhower"]["version"] = RaspberrypiPlatform.earle_toolchain[sys_type]
+            elif build_core == "amken3d":
+                self.frameworks["arduino"]["package"] = "framework-arduinorp2040"
+                self.packages["framework-arduino-mbed"]["optional"] = True
+                self.packages.pop("toolchain-gccarmnoneeabi", None)
+                self.packages["toolchain-rp2040-earlephilhower"]["optional"] = False
+                # Configure toolchain download link dynamically
+                self.packages["toolchain-rp2040-earlephilhower"]["version"] = RaspberrypiPlatform.earle_toolchain[sys_type]    
             else:
                 sys.stderr.write(
                     "Error! Unknown build.core value '%s'. Don't know which Arduino core package to use." % build_core)
